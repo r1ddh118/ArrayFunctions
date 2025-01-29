56da876ccd3683e5db422a9d965034fcb5fb88e4
Riddhi Poddar <riddhipoddar3@gmail.com>
2025-01-29 21:42:12 +0530

    uploaded the main.java file

---
+++ Main.java
@@ -0,0 +1,13 @@
+//Name: Riddhi Poddar
+//PRN: 23070126105
+//Batch: B2(2023-27)
+
+class Main {
+    public static void main(String args[]) {
+        // Creating an instance of ArrayFunctions to use its methods
+        ArrayFunctions functions = new ArrayFunctions();
+        
+        // Calling the method to process even and odd numbers
+        functions.evenOdd();
+    }
+}
